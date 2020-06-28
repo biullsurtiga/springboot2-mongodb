c1c3ed5aed1bffec6179855ddbb53fd178b15535
Severino Urtiga <urtigah@gmail.com>
2020-06-27 23:50:54 -0300

    Obtendo o post por id

---
+++ src/main/java/com/urtiga/springboot2mongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.urtiga.springboot2mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.urtiga.springboot2mongodb.domain.Post;
+import com.urtiga.springboot2mongodb.repository.PostRepository;
+import com.urtiga.springboot2mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id){
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
