6dcdd4a842fc475d34c1dcb3dff46f98198d5ecd
Severino Urtiga <urtigah@gmail.com>
2020-06-29 13:40:24 -0300

    Consulta simples com @Query

--- src/main/java/com/urtiga/springboot2mongodb/repository/PostRepository.java
+++ src/main/java/com/urtiga/springboot2mongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.urtiga.springboot2mongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.urtiga.springboot2mongodb.domain.Post;
@@ -10,5 +11,8 @@ import com.urtiga.springboot2mongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'tittle': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTittleContainingIgnoreCase(String text);
 }
